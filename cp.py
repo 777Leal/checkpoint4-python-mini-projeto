f640f2194ab9251f44ce066ce12ddef35d37ee60
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-04 20:10:36 -0300

    first commit

---
+++ cp.py
@@ -0,0 +1,33 @@
+servicos = {
+    "login": {
+        "criticidade": 3,
+        "descricao": "acesso ao sistema"
+    },
+    "pagamento": {
+        "criticidade": 5,
+        "descricao": "processamento de pagamentos"
+    },
+    "relatorio": {
+        "criticidade": 2,
+        "descricao": "geracao de relatorios"
+    },
+    "notificacoes":{
+        "criticidade": 1,
+        "descricao": "envio de avisos ao usuario"
+    },
+    "carrinho": {
+        "criticidade": 4,
+        "descricao": "carrinho de compras"
+    },"busca": {
+        "criticidade": 2,
+        "descricao": "busca de produtos"
+    },
+    "estoque": {
+        "criticidade": 3,
+        "descricao": "controle de estoque"
+    },
+    "api_externa": {
+        "criticidade": 4,
+        "descricao": "integracao com parceiros externos"
+    },
+}
